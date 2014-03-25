5aa8f1c5945ea147b763803d4cc3e8ad43aeeb18
Shannon Burns <shannon@codeshannon.com>
2014-03-24 17:16:44 -0700

    Working on webscraper, created lists of tags and lists of img urls

--- scraper.py
+++ scraper.py
@@ -1,15 +1,38 @@
 import requests
+from lxml import html
 from pyquery import PyQuery as pq
 
 url = 'http://icanhas.cheezburger.com/tag/dogs/' 
-
 r = requests.get(url)
-
 q = pq(r.text)
 
-doge = q('.post-asset-inner a')
+##### SOME HOW MAKE THIS SHIT INTO FUNCITONS ########
+filthyDoges = q('.post-asset-wrap .event-item-lol-image')
 
-print doge
+#Make a list of doges
+listOfDoges = []
+
+#Go through the dirty file of doge picturesfor doge in filthyDoge:     
+for doge in filthyDoges:
+    #add the src of each doge to the list 
+    listOfDoges.append(doge.get('src'))
+
+filthyDogeTags = q('.post-asset-wrap .unstyled')
+listOfLists = []
+#go through the dirty file of the list of lists
+for links in filthyDogeTags:
+    listOfTagsPerDoge = []
+#   for each list in the list loop through that list
+    for link in links:
+        #extract the link from the list
+        tag = link.text_content()
+        #extract the inner html of the link and add to list       
+        listOfTagsPerDoge.append(tag)
+
+    #add list of tags to list of lists
+    listOfLists.append(listOfTagsPerDoge)
+print listOfLists
+
 
 
 ### # is ID
