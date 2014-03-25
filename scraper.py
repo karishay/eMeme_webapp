e99b62db2a1124fe9d3f64c8638ad6cfdc88a179
Shannon Burns <shannon@codeshannon.com>
2014-03-24 18:13:34 -0700

    Need to traverse tree to find tags associated with src for memes

--- scraper.py
+++ scraper.py
@@ -5,17 +5,41 @@ from pyquery import PyQuery as pq
 url = 'http://icanhas.cheezburger.com/tag/dogs/' 
 r = requests.get(url)
 q = pq(r.text)
-
+#####################################################
 ##### SOME HOW MAKE THIS SHIT INTO FUNCITONS ########
+#####################################################
 filthyDoges = q('.post-asset-wrap .event-item-lol-image')
 
-#Make a list of doges
-listOfDoges = []
+def dogeWash(filthyDoges):
+    #Make a list of doges
+    listOfDoges = []
+    #Go through the dirty file of doge picturesfor doge in filthyDoge:     
+    for doge in filthyDoges:
+        #add the src of each doge to the list 
+        listOfDoges.append(doge.get('src'))
+    return listOfDoges
 
-#Go through the dirty file of doge picturesfor doge in filthyDoge:     
-for doge in filthyDoges:
-    #add the src of each doge to the list 
-    listOfDoges.append(doge.get('src'))
+
+def findDogeTag(listOfDoges):
+    dogeTags = []
+    for doge in listOfDoges:
+        #this is currently seeing the item in the list as a string instead of an object. transform it back into and object then traverse the tree
+        dogeDaddy = doge.getparent()
+        dogeAncestor = dogeDaddy.getparent().getparent().getparent().getchildren()[1]
+        sisterDoge = dogeAncestor.getchildren()[0].getchildren()[0].getchildren()[1].getchildren()[0].getchildren()
+        print sisterDoge
+
+#   find the parent of the src
+
+    #   locate the sibling tags of the src
+#   extract the innerHTML of the tag links
+#   happy dance
+#   return the list of tags for that src
+    return sisterDoge
+    # grandAunt = parent.getparent().getparent().getparent().getchildren()[1]
+# grandAunt.getchildren()[0].getchildren()[0].getchildren()[1].get("class")
+# grandAunt.getchildren()[0].getchildren()[0].getchildren()[1].getchildren()[0].getchildren()
+
 
 filthyDogeTags = q('.post-asset-wrap .unstyled')
 listOfLists = []
@@ -26,6 +50,7 @@ for links in filthyDogeTags:
     for link in links:
         #extract the link from the list
         tag = link.text_content()
+
         #extract the inner html of the link and add to list       
         listOfTagsPerDoge.append(tag)
 
@@ -34,6 +59,12 @@ for links in filthyDogeTags:
 print listOfLists
 
 
+# keys = listOfDoges
+# values = listOfLists
+
+# dictOfMemes = dict(zip(keys, values))
+
+# print dictOfMemes
 
 ### # is ID
 ### . is Class
