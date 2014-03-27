1c20a3e4964314a898269e43565a6fe81742b488
Shannon Burns <shannon@codeshannon.com>
2014-03-27 14:31:41 -0700

    fixed procfile

--- controller.py
+++ controller.py
@@ -3,7 +3,7 @@ from flask.ext.login import LoginManager, login_required, login_user, current_us
 from flask.ext.markdown import Markdown
 import config
 #may not need the forms.... maybe?
-import forms
+# import forms
 import model
 
 app = Flask(__name__)
@@ -19,57 +19,53 @@ def index():
     #load the landing page
     return render_template("index.html")
 
-@app.route("/", methods=["GET"])
-def index():
-    # remember to go back and make a login.html with jinja
-    return render_template("login.html")
 
-@app.route("/", methods=["POST"])
-def process_login():
-    email = request.form.get("email")
-    password = hash(request.form.get("password"))
+# @app.route("/", methods=["POST"])
+# def process_login():
+#     email = request.form.get("email")
+#     password = hash(request.form.get("password"))
 
-    user = model.authenticate(email, password)
-    if user:
-        session['user_id'] = user.id
-#        sessionUserId = model.Session.query(model.User).get(id)
-        return redirect(url_for("getMyRatings"))
-    else:
-        flash("Email or Password incorrect, grab your shit umbrella!")
-        return redirect(url_for("index"))
+#     user = model.authenticate(email, password)
+#     if user:
+#         session['user_id'] = user.id
+# #        sessionUserId = model.Session.query(model.User).get(id)
+#         return redirect(url_for("getMyRatings"))
+#     else:
+#         flash("Email or Password incorrect, grab your shit umbrella!")
+#         return redirect(url_for("index"))
 
-@app.route("/register", methods=["GET"])
-def registerPage():
-    return render_template("register.html")
+# @app.route("/register", methods=["GET"])
+# def registerPage():
+#     return render_template("register.html")
 
-@app.route("/register", methods=["POST"])
-def register():
-    #get all the things from the forms
-    email = request.form.get("email")
-    password = hash(request.form.get("password"))
-    passwordCheck = hash(request.form.get("password_verify"))
-    age = request.form.get("age")
-    zipcode = request.form.get("zipcode")
-    gender = request.form.get("gender")
+# @app.route("/register", methods=["POST"])
+# def register():
+#     #get all the things from the forms
+#     email = request.form.get("email")
+#     password = hash(request.form.get("password"))
+#     passwordCheck = hash(request.form.get("password_verify"))
+#     age = request.form.get("age")
+#     zipcode = request.form.get("zipcode")
+#     gender = request.form.get("gender")
 
-    #check if the passwords match
-    if password != passwordCheck:
-        flash("Learn how to type better, el suck!")
-        return redirect(url_for("registerPage"))
+#     #check if the passwords match
+#     if password != passwordCheck:
+#         flash("Learn how to type better, el suck!")
+#         return redirect(url_for("registerPage"))
 
-    else:
-        if model.checkEmail(email):
-            flash("Biotch, you already gots an account.")
-            return redirect(url_for("registerPage"))
-        else:
-            newUser = model.User(email = email, 
-                           password = password, 
-                           age = age, 
-                           zipcode = zipcode, 
-                           gender = gender)
-            model.Session.add(newUser)
-            model.Session.commit()
-            return redirect(url_for("index"))
+#     else:
+#         if model.checkEmail(email):
+#             flash("Biotch, you already gots an account.")
+#             return redirect(url_for("registerPage"))
+#         else:
+#             newUser = model.User(email = email, 
+#                            password = password, 
+#                            age = age, 
+#                            zipcode = zipcode, 
+#                            gender = gender)
+#             model.Session.add(newUser)
+#             model.Session.commit()
+#             return redirect(url_for("index"))
 ###some other stuff###
 if __name__ == "__main__":
     app.run(debug=True)
