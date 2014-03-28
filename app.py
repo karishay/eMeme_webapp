86c354023bf6d9460179c774d0df966baea5f0fc
Shannon Burns <shannon@codeshannon.com>
2014-03-27 18:38:28 -0700

    ahh

--- app.py
+++ app.py
@@ -22,4 +22,5 @@ def index():
 
 ###some other stuff###
 if __name__ == "__main__":
-    app.run(debug=True, port = int(os.environ.get("PORT", 3000)))
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
