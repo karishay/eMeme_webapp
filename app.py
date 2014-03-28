18b6ae8a859c47e80b9bbeaadbfd9a8d0ddc405b
Shannon Burns <shannon@codeshannon.com>
2014-03-27 18:26:02 -0700

    Fixing heroku deployment and added graph db model

--- app.py
+++ app.py
@@ -21,4 +21,4 @@ def index():
 
 ###some other stuff###
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port = os.environ.get("PORT", 3000))
---
+++ graphModel.py
@@ -0,0 +1,31 @@
+from py2neo import neo4j
+from py2neo import node, rel
+
+graph_db = neo4j.GraphDatabaseService("http://localhost:7474/db/data/")
+
+#############################################################
+################## Class Declarations Here ##################
+#############################################################
+
+
+
+
+#############################################################
+##################### functions #############################
+#############################################################
+
+def createGraph():
+    #make a function to create graph db
+    pass
+
+#define a function the recieves the assets from scraper
+#and creates the nodes and relationships in db
+
+
+
+def main():
+    """In case we need this for something."""
+
+
+if __name__ == "__main__":
+    main()
