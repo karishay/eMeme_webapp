518a5abc727d5edeb4fe118921d543aea3a879c6
Shannon Burns <shannon@codeshannon.com>
2014-04-01 11:46:31 -0700

    Altered functionality to add weight of TAGGED relationships.

--- graphModel.py
+++ graphModel.py
@@ -7,76 +7,48 @@ graph_db = neo4j.GraphDatabaseService("http://localhost:7476/db/data/")
 store = ogm.Store(graph_db)
 
 #############################################################
-##################### functions #############################
+####### functions to create Nodes and Relationships #########
 #############################################################
 
-###----------------These are Relationships----------------###
-
-
-###---------------------These are Nodes-------------------###
-#define a function that creates a node
 def createImgNode(memeDict):
+    """Description: Creates img nodes and assigns appropriated tagged 
+                    relationships to tag nodes.
+       Param: memeDict, dictionary, keys are urls and values are tags
+       Returns: None"""
+
     urls = memeDict.keys()
     for url in urls:
-        print "url is %s" % url
         imgNode, = graph_db.create({"imgSrc":url, "aWeight":0})
         imgNode.add_labels("IMG")
 
         for tag in memeDict[url]:
-            #if its in the db then add one to the weight
-            print "tag is: %s" % tag
             tagNode = getTagNode(tag)
-            #rel((imgNode, ("TAGGED", {"aWeight":1}), tagNode))
-            graph_db.create(rel(imgNode, "TAGGED", tagNode))
+            graph_db.create(rel(imgNode, ("TAGGED", {"aWeight": tagNode[1]}), tagNode[0][0]))
 
-# define a function that returns a tag node
 def getTagNode(tag):
+    """Description: Creates and lables tag nodes 
+       Param: tag, string, characteristic of image
+       Returns: a tuple with a node in list form and a weight integer"""
+
     tags = graph_db.get_or_create_index(neo4j.Node, "Tags")
-    t = tags.get_or_create("tagName", tag, {"tagName": tag})
-    t.add_labels("TAG")
-    return t 
+    aWeight = 1
+    #define the tag query
+    perhapsTag = tags.get("tagName", tag)
+    if len(perhapsTag) == 1:
+        perhapsProp = perhapsTag[0]._properties
+        pTagName = perhapsProp.get("tagName")
+        aWeight += 1
+        t = perhapsTag
+    else:
+        t = tags.get_or_create("tagName", tag, {"tagName": tag})
+        t.add_labels("TAG")
+        t = [t]
+    return (t, aWeight)
 
 
 #############################################################
 ##################### functions #############################
 #############################################################
-#call functions from scraper that return things with scraper.blah
-# def createGraph(memeDict):
-    # #make a function to create graph db
-    # #create a list of nodes
-
-    # images = []
-    # tags = []
-    # for img in memeDict:
-    #     #so this part is working properly as it has added images notes to the 
-    #     imgNode = Img(imgSrc=img, aggregateImgWeight=0).save()
-    #     imgNode.refresh()
-        
-    #     for tag in memeDict[img]:
-    #         #check if tag exists already in the database (this part is not working)
-    #         if not Tag.index.search(tagName=tag):
-    #                  #created the tag 
-    #             tagNode = Tag(tagName=tag).save()
-    #             tagNode.refresh()
-    #             print tagNode
-    #             #connect it to the img with the relationship (write this one as a cypher query)
-    #             query = "MATCH (i:'{imgNode}'), (t:'{tagNode}') CREATE (i)-[r:TAGGED]->(t) RETURN r"
-    #             params = {'imgNode': dir(imgNode.imgIndex), 'tagNode':tagNode.tagName}
-    #             print params
-    #             taggedRelationship = cypher.execute(graph_db, query, params)
-    #             print taggedRelationship
-
-         
-    #         else:
-    #             query = "MATCH ()-[r:TAGGED]-() return r"
-    #             taggedRelationship = cypher.execute(graph_db, query)
-    #             print "This is the tagged relationship[0]", taggedRelationship[0]
-    #             # cypher_query.append(query, params)
-    #             #params = dictionary of parameters (properties)
-    #             # taggedRelationship.aggregateImgTagWeight += 1
-    #             # taggedRelationship.commit
-    # return "The createGraph function ran! Check DB to see if it worked."
-
 
 
 def main():
